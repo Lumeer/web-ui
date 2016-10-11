d5724d2a9c630645d789f6cbc638203d7613847d
Martin Večeřa <marvenec@gmail.com>
2016-10-11 23:16:23 +0200

    demo web page

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+(function(){
+  $(window).scroll(function () {
+      var top = $(document).scrollTop();
+      $('.splash').css({
+        'background-position': '0px -'+(top/3).toFixed(2)+'px'
+      });
+      if(top > 50)
+        $('#home > .navbar').removeClass('navbar-transparent');
+      else
+        $('#home > .navbar').addClass('navbar-transparent');
+  });
+
+  $("a[href='#']").click(function(e) {
+    e.preventDefault();
+  });
+
+})();
