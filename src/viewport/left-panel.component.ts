ac646bb1d0544494778f4286760e773726eb900f
Karel Hala <khala@redhat.com>
2016-11-06 16:34:03 +0100

    Make left panel collapsable when collapsing left panel, content will take 11/12 width of screen, navigation will be 1/12. When navigation will be expanded it will take 2/12 and content 10/12.

--- src/viewport/left-panel.component.ts
+++ src/viewport/left-panel.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'left-panel',
   template: require('./left-panel.component.html'),
   styles: [ require('./left-panel.component.scss').toString() ]
 })
-export class LeftPanelComponent { }
+export class LeftPanelComponent {
+  @Input() public collapsedValue: boolean;
+}
