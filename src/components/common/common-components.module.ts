0b1ecfe907948fdfd76267a55409ca7c8c929008
Karel Hala <karelhala.91@gmail.com>
2016-11-27 19:23:12 +0100

    Change selector for editable, move values to tag options

--- src/components/common/common-components.module.ts
+++ src/components/common/common-components.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {TagInputComponent} from './tag-input/tag-input.component';
 import {BrowserModule} from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { Ng2AutoCompleteModule } from 'ng2-auto-complete';
 import {HttpModule} from '@angular/http';
 import {AutoCompleteComponent} from './auto-complete/auto-complete.component';
 import {LumEditable} from './auto-complete/lum-editable';
@@ -11,7 +10,6 @@ import {LumEditable} from './auto-complete/lum-editable';
   imports: [
     BrowserModule,
     FormsModule,
-    Ng2AutoCompleteModule,
     HttpModule
   ],
   declarations: [
