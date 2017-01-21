203516f773ae752d60d5cf303f035c4417e56db2
Martin Večeřa <marvenec@gmail.com>
2017-01-21 14:58:15 +0100

    fixed path deployment, keycloak configuration

--- src/main/scripts/webpack.config.js
+++ src/main/scripts/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: __dirname + '/dist',
     filename: "js/[name].bundle.js",
-    publicPath: '/'
+    publicPath: ''
   },
   resolve: {
     extensions: ['.js', '.ts']
