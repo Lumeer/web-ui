7501edc37f87092f4202705a1dfc3b53188b88e1
Šimon Kocúrek <simon.kocurek@gmail.com>
2017-09-06 14:24:18 +0200

    Update Font Awesome icons #LMR-338

--- src/main/scripts/src/vendor.ts
+++ src/main/scripts/src/vendor.ts
@@ -7,14 +7,20 @@ import '@angular/http';
 import '@angular/router';
 import '@angular/forms';
 import '@angular/animations';
+
 import './js/custom.js';
+
+import './font-awesome/solid.min.js';
+import './font-awesome/regular.min.js';
+import './font-awesome/brands.min.js';
+import './font-awesome/light.min.js';
+import './font-awesome/fontawesome.min.js';
+
 import 'socket.io-client';
 import 'angular2-drag-scroll';
 import 'angular2-notifications';
 import 'ngx-perfect-scrollbar';
 import 'ng2-webstorage';
-// import 'core-js/es6';
-// import 'core-js/es7/reflect';
-// import 'zone.js/dist/zone';
+
 window['Minigrid'] = require('../node_modules/minigrid/dist/minigrid.min.js');
 window['Keycloak'] = require('./js/keycloak.js');
