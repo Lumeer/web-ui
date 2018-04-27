b43cf83bb18abe50c5b26e66ae59c63cb2ef67da
Jakub Rodák <kubedo8@gmail.com>
2018-04-27 18:39:13 +0200

    resource header components

---
+++ src/app/shared/resource/header/resource-header.component.ts
@@ -0,0 +1,49 @@
+/*
+ * Lumeer: Modern Data Definition and Processing Platform
+ *
+ * Copyright (C) since 2017 Answer Institute, s.r.o. and/or its affiliates.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+import {ResourceType} from '../../../core/model/resource-type';
+import {ResourceModel} from '../../../core/model/resource.model';
+
+@Component({
+  selector: 'resource-header',
+  templateUrl: './resource-header.component.html'
+})
+export class ResourceHeaderComponent implements OnInit {
+
+  @Input() public resourceType: ResourceType;
+
+  @Output() codeChange: EventEmitter<string> = new EventEmitter();
+  @Output() nameChange: EventEmitter<string> = new EventEmitter();
+  @Output() descriptionChange: EventEmitter<string> = new EventEmitter();
+  @Output() back = new EventEmitter();
+
+  public resource: ResourceModel;
+
+
+  public ngOnInit() {
+    this.subscribeData();
+  }
+
+  private subscribeData() {
+
+  }
+
+}
