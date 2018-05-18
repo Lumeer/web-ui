917a8d1ddac8c9127d290fed9ce94d12574f91c0
Martin Večeřa <marvenec@gmail.com>
2018-05-18 15:13:04 +0200

    Organize shared components into modules

---
+++ src/app/shared/preview-results/preview-results.module.ts
@@ -0,0 +1,39 @@
+/*
+ * Lumeer: Modern Data Definition and Processing Platform
+ *
+ * Copyright (C) since 2017 Answer Institute, s.r.o. and/or its affiliates.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {PreviewResultsComponent} from "./preview-results.component";
+import {PreviewResultsTableComponent} from "./preview-results-table/preview-results-table.component";
+import {PreviewResultsTabsComponent} from "./preview-results-tabs/preview-results-tabs.component";
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    PreviewResultsComponent,
+    PreviewResultsTableComponent,
+    PreviewResultsTabsComponent
+  ],
+  exports: [
+    PreviewResultsComponent
+  ]
+})
+export class PreviewResultsModule { }
