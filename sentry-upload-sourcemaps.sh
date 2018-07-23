99a554b8b804117e4bb34551ca9f39a1e4590d1e
Tomáš Livora <livoratom@gmail.com>
2018-07-23 20:39:18 +0200

    Upload JavaScript source maps to Sentry

---
+++ sentry-upload-sourcemaps.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+export SENTRY_ORG=answer-institute-sro
+export SENTRY_PROJECT=lumeerio
+
+if [ "$LUMEER_ENV" != "production" ]
+then
+  exit 0
+fi
+
+if [ -z "$BUILD_NUMBER" ]
+then
+  echo -e "BUILD_NUMBER is not defined."
+  exit 1
+fi
+
+if [ -z "$PUBLIC_PATH" ]
+then
+  echo -e "PUBLIC_PATH is not defined."
+  exit 1
+fi
+
+if [ -z "$SENTRY_AUTH_TOKEN" ]
+then
+  echo -e "SENTRY_AUTH_TOKEN is not defined."
+  exit 1
+fi
+
+./node_modules/.bin/sentry-cli releases new $BUILD_NUMBER \
+  && ./node_modules/.bin/sentry-cli releases files $BUILD_NUMBER upload-sourcemaps --url-prefix https://get.lumeer.io$PUBLIC_PATH ./dist/lumeer
