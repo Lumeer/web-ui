d94dad341397577c571fdb6ec9c9ce2dfbe242aa
Tomáš Livora <livoratom@gmail.com>
2018-07-30 11:55:40 +0200

    Hide new project button if limits exceeded #LMR-1037

--- src/app/shared/top-panel/resource-menu/pipes/can-create-resource.pipe.ts
+++ src/app/shared/top-panel/resource-menu/pipes/can-create-resource.pipe.ts
@@ -18,18 +18,18 @@
  */
 
 import {Pipe, PipeTransform} from '@angular/core';
-
+import {Store} from '@ngrx/store';
+import {combineLatest, Observable, of} from 'rxjs';
+import {filter, map, mergeMap} from 'rxjs/operators';
 import {ResourceType} from '../../../../core/model/resource-type';
 import {ResourceModel} from '../../../../core/model/resource.model';
-import {of, Observable} from 'rxjs';
-import {AppState} from '../../../../core/store/app.state';
-import {Store} from '@ngrx/store';
-import {map, mergeMap} from 'rxjs/operators';
-import {selectCurrentUser} from '../../../../core/store/users/users.state';
-import {selectOrganizationById} from '../../../../core/store/organizations/organizations.state';
-import {ProjectModel} from '../../../../core/store/projects/project.model';
-import {PermissionsPipe} from '../../../pipes/permissions.pipe';
 import {Role} from '../../../../core/model/role';
+import {AppState} from '../../../../core/store/app.state';
+import {selectOrganizationById} from '../../../../core/store/organizations/organizations.state';
+import {selectServiceLimitsByOrganizationId} from '../../../../core/store/organizations/service-limits/service-limits.state';
+import {ProjectModel} from '../../../../core/store/projects/project.model';
+import {selectCurrentUser} from '../../../../core/store/users/users.state';
+import {PermissionsPipe} from '../../../pipes/permissions.pipe';
 
 const allowedEmails = ['support@lumeer.io', 'martin@vecerovi.com', 'kubedo8@gmail.com', 'livoratom@gmail.com'];
 
@@ -38,19 +38,25 @@ const allowedEmails = ['support@lumeer.io', 'martin@vecerovi.com', 'kubedo8@gmai
 })
 export class CanCreateResourcePipe implements PipeTransform {
 
-  public constructor(private store: Store<AppState>,
+  public constructor(private store$: Store<AppState>,
                      private permissionsPipe: PermissionsPipe) {
   }
 
-  public transform(resource: ResourceModel, type: ResourceType): Observable<boolean> {
+  public transform(resource: ResourceModel, type: ResourceType, projects: ProjectModel[]): Observable<boolean> {
     if (type === ResourceType.Organization) {
-      return this.store.select(selectCurrentUser).pipe(
+      return this.store$.select(selectCurrentUser).pipe(
         map(user => allowedEmails.includes(user.email))
       );
     } else if (type === ResourceType.Project) {
       const project = resource as ProjectModel;
-      return this.store.select(selectOrganizationById(project.organizationId)).pipe(
-        mergeMap(organization => this.permissionsPipe.transform(organization, Role.Write))
+      return combineLatest(
+        this.store$.select(selectOrganizationById(project.organizationId)),
+        this.store$.select(selectServiceLimitsByOrganizationId(project.organizationId))
+      ).pipe(
+        filter(([organization, serviceLimits]) => !!organization && !!serviceLimits),
+        mergeMap(([organization, serviceLimits]) => this.permissionsPipe.transform(organization, Role.Write).pipe(
+          map(allowed => allowed && projects.length < serviceLimits.projects)
+        ))
       );
     }
     return of(true);
