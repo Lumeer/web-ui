e16981256ed556e611a43ac677134a6768a5de1c
Martin Večeřa <marvenec@gmail.com>
2018-08-15 09:31:05 +0200

    Add backend startup/shutdown procedure

--- travis-start-engine.sh
+++ travis-start-engine.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-echo "Downloading engine..."
-git clone git@github.com:Lumeer/engine.git
-cd engine
+$ORIG=$(pwd)
+
+if [ -d ~/.engine ]; then
+  echo "Pulling latest engine updates..."
+  cd ~/.engine
+  git checkout devel
+  git pull origin devel
+else
+  echo "Downloading engine..."
+  git clone git@github.com:Lumeer/engine.git ~/.engine
+  git checkout devel
+  cd ~/.engine
+fi
 
 echo "Building engine..."
 mvn install -DskipTests
@@ -11,7 +21,7 @@ echo "Starting engine..."
 export SKIP_LIMITS=true
 export SKIP_SECURITY=true
 mvn -s settings.xml wildfly:run -PstartEngine &
-echo $! > ../../engine.pid
+echo $! > $ORIG/engine.pid
 
 echo "Waiting for engine to start..."
 while ! test -f "target/wildfly-run/wildfly-13.0.0.Final/standalone/tmp/startup-marker"; do
@@ -20,3 +30,5 @@ done
 sleep 5
 
 echo "Engine started!"
+
+cd $ORIG
