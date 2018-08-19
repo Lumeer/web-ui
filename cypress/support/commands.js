52abf4d5f6454a37b6d0a28cb79cb9baf3404de7
Martin Večeřa <marvenec@gmail.com>
2018-08-19 15:25:52 +0200

    Update Cypress version, add test auth, simple hello world test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,49 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+import auth0 from 'auth0-js';
+
+Cypress.Commands.add('login', () => {
+  const auth = new auth0.WebAuth({
+    audience: Cypress.env('engineServer'),
+    domain: Cypress.env('auth0Domain'),
+    clientID: Cypress.env('auth0ClientId'),
+    redirectUri: Cypress.config('baseUrl') + '/ui/auth',
+    responseType: 'token id_token',
+    scope: 'openid email profile name username groups roles',
+  })
+
+  // first make the browser open our app so that we use its local storage
+  cy.visit('/ui/auth')
+  // login via auth0 API
+  auth.login({username: 'user1@lumeer.io', password: 'userOne123'})
+
+  // wait for the token
+  cy.window().its('localStorage').invoke('getItem', 'auth_id_token').should('exist')
+});
+
+// allows to perform actions using access token like calling backend API
+Cypress.Commands.add('withToken', (fn) => {
+  cy.window().its('localStorage').invoke('getItem', 'auth_access_token').then((token) => fn(token))
+})
+
+Cypress.Commands.add('logout', () => {
+  // remove tokens from local storage
+  cy.window().its('localStorage').then((localStorage) => {
+    localStorage.removeItem('auth_id_token')
+    localStorage.removeItem('auth_access_token')
+  })
+});
+
+Cypress.Commands.add('dismissAgreement', () => {
+  cy.withToken((token) => {
+    // make sure to pass license agreement
+    cy.request({
+      method: 'PATCH',
+      url: Cypress.env('engineUrl') + 'rest/users/current',
+      body: {
+        'agreement': true, 'newsletter': true
+      },
+      auth: {
+        bearer: token
+      }
+    })
+  })
+})
