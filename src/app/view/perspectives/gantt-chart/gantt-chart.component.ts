9eedd94326c853d42e09e12be38ec2424bf52cdd
Viktoria Mikoova <viktoria.mikoova@gmail.com>
2018-11-01 22:09:19 +0100

    Frappe Gantt correct set up

--- src/app/view/perspectives/gantt-chart/gantt-chart.component.ts
+++ src/app/view/perspectives/gantt-chart/gantt-chart.component.ts
@@ -30,6 +30,8 @@ import {selectPerspectiveViewConfig} from '../../../core/store/views/views.state
 //import {DEFAULT_MAP_ID, selectMapById} from "../../../core/store/maps/maps.state";
 import {map} from 'rxjs/operators';
 
+import * as frappeGantt from 'frappe-gantt';
+
 @Component({
   selector: 'gantt-chart',
   templateUrl: './gantt-chart.component.html',
@@ -57,6 +59,7 @@ export class GanttChartComponent implements OnInit {
     this.collections$ = this.store$.pipe(select(selectCollectionsByQuery));
     this.documents$ = this.store$.pipe(select(selectDocumentsByQuery));
     //this.bindCalendar(this.calendarId);
+    this.showGantt();
   }
 
   private bindValidQuery() {
@@ -64,6 +67,87 @@ export class GanttChartComponent implements OnInit {
       select(selectQuery),
       map(query => query && query.collectionIds && query.collectionIds.length > 0)
     );
+  }
+
+  private showGantt() {
+
+    const tasks = [
+      {
+        start: '2018-10-01',
+        end: '2018-10-08',
+        name: 'Redesign website',
+        id: 'Task 0',
+        progress: 20
+      },
+      {
+        start: '2018-10-03',
+        end: '2018-10-06',
+        name: 'Write new content',
+        id: 'Task 1',
+        progress: 5,
+        dependencies: 'Task 0'
+      },
+      {
+        start: '2018-10-04',
+        end: '2018-10-08',
+        name: 'Apply new styles',
+        id: 'Task 2',
+        progress: 10,
+        dependencies: 'Task 1'
+      },
+      {
+        start: '2018-10-08',
+        end: '2018-10-09',
+        name: 'Review',
+        id: 'Task 3',
+        progress: 5,
+        dependencies: 'Task 2'
+      },
+      {
+        start: '2018-10-08',
+        end: '2018-10-10',
+        name: 'Deploy',
+        id: 'Task 4',
+        progress: 0,
+        dependencies: 'Task 2'
+      },
+      {
+        start: '2018-10-11',
+        end: '2018-10-11',
+        name: 'Go Live!',
+        id: 'Task 5',
+        progress: 0,
+        dependencies: 'Task 4',
+        custom_class: 'bar-milestone'
+      },
+      {
+        start: '2014-01-05',
+        end: '2019-10-12',
+        name: 'Long term task',
+        id: 'Task 6',
+        progress: 0
+      }
+    ];
+
+    //const because of push (let and var are rejected)
+    const gantt_chart = new frappeGantt.default('.gantt-target', tasks, {
+      // console logs are forbidden for push
+      /*on_click: function (task) {
+        console.log(task);
+      },
+      on_date_change: function(task, start, end) {
+        console.log(task, start, end);
+      },
+      on_progress_change: function(task, progress) {
+        console.log(task, progress);
+      },
+      on_view_change: function(mode) {
+        console.log(mode);
+      },*/
+      view_mode: 'Month',
+      language: 'en'
+    });
+   // console.log(gantt_chart);
   }
 
 }
