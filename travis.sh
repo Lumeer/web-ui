a7fa04f16eab2f4811a15c3d38fb6f0f1125287f
Tomáš Livora <livoratom@gmail.com>
2018-11-10 11:48:01 +0000

    Use Docker image with browsers for CircleCI #LMR-1234

--- travis.sh
+++ travis.sh
@@ -30,16 +30,6 @@ error_handler() {
 #bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
 #PING_LOOP_PID=$!
 
-echo "Checking formatting..."
-npm run format:check
-
-echo "Linting..."
-npm run lint #>> $BUILD_OUTPUT 2>&1
-#dump_output
-
-echo "Unit testing..."
-npm run test
-
 if false; then
 
   echo "Starting UI..."
