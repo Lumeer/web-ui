e2b0150cfb11073a98d8dd70bf374207c022827d
Tomáš Livora <livoratom@gmail.com>
2018-11-16 14:55:06 +0000

    Control bundle size in Travis CI #LMR-1249
    * run full production build
    * fix ERROR in ./node_modules/plotly.js/src/lib/polygon.js

--- travis.sh
+++ travis.sh
@@ -30,7 +30,7 @@ error_handler() {
 #bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
 #PING_LOOP_PID=$!
 
-echo "Starting UI..."
+echo "Starting frontend..."
 npm run start:aot & #>> $BUILD_OUTPUT 2>&1 &
 while ! curl --output /dev/null --silent -r 0-0 --fail "http://localhost:7000/ui"; do
   sleep 3
@@ -40,11 +40,11 @@ echo "Starting backend..."
 ./travis-start-engine.sh #>> $BUILD_OUTPUT 2>&1
 #dump_output
 
-echo "Testing UI..."
+echo "Running E2E tests..."
 npm run cypress:run --  --record --key b43d988f-5145-4a2b-9df3-ce3b1607f203 #>> $BUILD_OUTPUT 2>&1
 #dump_output
 
-echo "Stopping UI..."
+echo "Stopping frontend..."
 pkill npm
 
 echo "Stopping backend..."
@@ -53,3 +53,9 @@ echo "Stopping backend..."
 
 # nicely terminate the ping output loop
 #kill $PING_LOOP_PID
+
+echo "Running production build..."
+LUMEER_ENV=production SKIP_SENTRY_UPLOAD=true mvn clean install
+
+echo "Checking bundle sizes..."
+npm run bundlesize
