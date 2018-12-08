e4658a1776c1c45221b6404e5cbe6cbd9f2fcfc1
Viktoria Mikoova <viktoria.mikoova@gmail.com>
2018-12-08 18:53:21 +0100

    initial gantt chart page and collection color set for gantt

--- src/app/view/perspectives/gantt-chart/gantt-chart-visualization/gantt-chart-visualization.component.ts
+++ src/app/view/perspectives/gantt-chart/gantt-chart-visualization/gantt-chart-visualization.component.ts
@@ -18,6 +18,9 @@ import {
   GanttChartConfig,
 } from '../../../../core/store/gantt-charts/gantt-chart.model';
 import * as frappeGantt from 'frappe-gantt';
+
+declare let $: any;
+
 @Component({
   selector: 'gantt-chart-visualization',
   templateUrl: './gantt-chart-visualization.component.html',
@@ -90,7 +93,44 @@ export class GanttChartVisualizationComponent implements OnChanges {
       if (tasks.length > 0) {
         this.gantt_chart = new frappeGantt.default('#ganttChart', tasks);
         this.gantt_chart.change_view_mode(this.config.mode);
+
+        let textColor = this.getContrastYIQ(this.collection.color.substring(1, 6));
+        $('.gantt .bar').css('fill', this.collection.color);
+        $('.gantt .bar-label').css('fill', textColor);
+        if (textColor == 'black') {
+          $('.gantt .bar-progress').css('fill', this.shadeColor2(this.collection.color, 50));
+        } else {
+          $('.gantt .bar-progress').css('fill', this.shadeColor2(this.collection.color, 0.3));
+        }
       }
     }
+  }
+
+  private getContrastYIQ(hexcolor) {
+    var r = parseInt(hexcolor.substr(0, 2), 16);
+    var g = parseInt(hexcolor.substr(2, 2), 16);
+    var b = parseInt(hexcolor.substr(4, 2), 16);
+    var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+    return yiq >= 128 ? 'black' : 'white';
+  }
+
+  private shadeColor2(color, percent) {
+    var f = parseInt(color.slice(1), 16),
+      t = percent < 0 ? 0 : 255,
+      p = percent < 0 ? percent * -1 : percent,
+      R = f >> 16,
+      G = (f >> 8) & 0x00ff,
+      B = f & 0x0000ff;
+    return (
+      '#' +
+      (
+        0x1000000 +
+        (Math.round((t - R) * p) + R) * 0x10000 +
+        (Math.round((t - G) * p) + G) * 0x100 +
+        (Math.round((t - B) * p) + B)
+      )
+        .toString(16)
+        .slice(1)
+    );
   }
 }
