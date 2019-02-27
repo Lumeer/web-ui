4a6ed1d2b529e3321fcecd953967b82fb8414f79
Martin Večeřa <mvecera@redhat.com>
2019-02-27 19:14:49 +0100

    Add i18n to app tour

--- src/app/shared/top-panel/user-panel/user-menu/user-menu.component.ts
+++ src/app/shared/top-panel/user-panel/user-menu/user-menu.component.ts
@@ -38,6 +38,7 @@ import PatchCurrentUser = UsersAction.PatchCurrentUser;
 import {selectAllCollections} from '../../../../core/store/collections/collections.state';
 import {isDialogPathInUrl} from '../../../../dialog/dialog.utils';
 import {Collection} from '../../../../core/store/collections/collection';
+import {I18n} from '@ngx-translate/i18n-polyfill';
 
 @Component({
   selector: 'user-menu',
@@ -66,7 +67,7 @@ export class UserMenuComponent {
     private dialogService: DialogService,
     private store$: Store<AppState>,
     private router: Router,
-    private route: ActivatedRoute
+    private i18n: I18n
   ) {}
 
   public ngOnInit() {
@@ -76,7 +77,7 @@ export class UserMenuComponent {
       onReset: () => this.dismissWizard(),
     });
     setTimeout(() => {
-      // trick to find element in the whole page
+      // trick to find elements in the whole page
       this.defineSteps();
     });
 
@@ -92,7 +93,7 @@ export class UserMenuComponent {
       .pipe(
         filter(([user, collections, url]) => !!user && !!collections && !!url),
         filter(([user, collections, url]) => this.isViewSearchAll(url)),
-        filter(([user, collections, url]) => !user.wizardDismissed && collections.length >= 0),
+        filter(([user, collections, url]) => !user.wizardDismissed && collections.length === 0),
         first()
       )
       .subscribe(next => this.startTour());
@@ -157,61 +158,103 @@ export class UserMenuComponent {
       {
         element: '[data-tour="collection-add"]',
         popover: {
-          title: 'Welcome to Lumeer',
-          description:
-            'Lumeer organizes your information in collections (i.e. categories) of similar records. Later, you can use this button to add your first collection. Then you can open the collection by simply clicking on it.',
+          title: this.i18n({
+            id: 'appTour.title.welcome',
+            value: 'Welcome to Lumeer',
+          }),
+          description: this.i18n({
+            id: 'appTour.description.welcome',
+            value:
+              '(1/7) Lumeer organizes your information in collections (i.e. categories) of similar records. Later, you can use this button to add your first collection. Then you can open the collection by simply clicking on it.',
+          }),
           position: 'right',
         },
       },
       {
         element: '[data-tour="logo"]',
         popover: {
-          title: 'Get home',
-          description:
-            'By clicking on the Lumeer icon, you can always return to this page where you can best find and access your stored data.',
+          title: this.i18n({
+            id: 'appTour.title.home',
+            value: 'Get home',
+          }),
+          description: this.i18n({
+            id: 'appTour.description.home',
+            value:
+              'By clicking on the Lumeer icon, you can always return to this page where you can best find and access your stored data.',
+          }),
           position: 'right',
         },
       },
       {
         element: '[data-tour="search-box"]',
         popover: {
-          title: 'Search for information',
-          description:
-            'The best way to locate your stored information is to search for it. Lumeer will guide you, just start typing in the search box.',
+          title: this.i18n({
+            id: 'appTour.title.search',
+            value: 'Search for information',
+          }),
+          description: this.i18n({
+            id: 'appTour.description.search',
+            value:
+              'The best way to locate your stored information is to search for it. Lumeer will guide you, just start typing in the search box.',
+          }),
           position: 'bottom',
         },
       },
       {
         element: '[data-tour="perspective"]',
         popover: {
-          title: 'Perspectives',
-          description:
-            'When you open your collection or search results, try selecting a different visual perspective. This is similar to changing glasses through which you can see your information in various ways.',
+          title: this.i18n({
+            id: 'appTour.title.perspectives',
+            value: 'Perspectives',
+          }),
+          description: this.i18n({
+            id: 'appTour.description.perspectives',
+            value:
+              'When you open your collection or search results, try selecting a different visual perspective. This is similar to changing glasses through which you can see your information in various ways.',
+          }),
           position: 'right',
         },
       },
       {
         element: '[data-tour="view"]',
         popover: {
-          title: 'Views and sharing',
-          description:
-            "You might connect multiple collections in relations, select a Table perspective and hide some columns for example. You don't need to repeat the steps each time. Just give your view, or page if you will, a name and store it. Later you can access the stored view on home page or you can share the view with your colleagues.",
+          title: this.i18n({
+            id: 'appTour.title.views',
+            value: 'Views and sharing',
+          }),
+          description: this.i18n({
+            id: 'appTour.description.views',
+            value:
+              "You might connect multiple collections in relations, select a Table perspective and hide some columns for example. You don't need to repeat the steps each time. Just give your view, or page if you will, a name and store it. Later you can access the stored view on home page or you can share the view with your colleagues.",
+          }),
           position: 'bottom',
         },
       },
       {
         element: '[data-tour="video-menu"]',
         popover: {
-          title: 'Help with Lumeer',
-          description: 'Here you can find a list of one-minute videos that explain controls on each page.',
+          title: this.i18n({
+            id: 'appTour.title.videos',
+            value: 'Help with Lumeer',
+          }),
+          description: this.i18n({
+            id: 'appTour.description.videos',
+            value: 'Here you can find a list of one-minute videos that explain controls on each page.',
+          }),
           position: 'left',
         },
       },
       {
         element: '[data-tour="user-menu"]',
         popover: {
-          title: 'Return to this Tour',
-          description: 'You can always return to this tour by invoking it from the user menu.',
+          title: this.i18n({
+            id: 'appTour.title.userMenu',
+            value: 'Return to this Tour',
+          }),
+          description: this.i18n({
+            id: 'appTour.description.userMenu',
+            value: 'You can always return to this tour by invoking it from the user menu.',
+          }),
           position: 'left',
         },
       },
