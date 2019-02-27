efb8a1d9f27e890cd1fb2681cf25af3bb16465b5
Martin Večeřa <marvenec@gmail.com>
2019-02-27 21:53:33 +0100

    Made tour react to all desired situations

--- src/app/shared/top-panel/user-panel/user-menu/user-menu.component.ts
+++ src/app/shared/top-panel/user-panel/user-menu/user-menu.component.ts
@@ -23,22 +23,20 @@ import {User} from '../../../../core/store/users/user';
 import {environment} from '../../../../../environments/environment';
 import {AuthService} from '../../../../auth/auth.service';
 import {AppState} from '../../../../core/store/app.state';
-import {selectUrl} from '../../../../core/store/navigation/navigation.state';
+import {selectUrl, selectWorkspace} from '../../../../core/store/navigation/navigation.state';
 import {DialogService} from '../../../../dialog/dialog.service';
 import {selectCurrentUser} from '../../../../core/store/users/users.state';
 import {ServiceLimitsAction} from '../../../../core/store/organizations/service-limits/service-limits.action';
 import {selectServiceLimitsByWorkspace} from '../../../../core/store/organizations/service-limits/service-limits.state';
-import {filter, first, map, tap} from 'rxjs/operators';
+import {filter, first, map} from 'rxjs/operators';
 import {ServiceLevelType} from '../../../../core/dto/service-level-type';
 import {Workspace} from '../../../../core/store/navigation/workspace';
-import {ActivatedRoute, NavigationStart, Router} from '@angular/router';
+import {Router} from '@angular/router';
 import * as Driver from 'driver.js';
 import {UsersAction} from '../../../../core/store/users/users.action';
-import PatchCurrentUser = UsersAction.PatchCurrentUser;
 import {selectAllCollections} from '../../../../core/store/collections/collections.state';
-import {isDialogPathInUrl} from '../../../../dialog/dialog.utils';
-import {Collection} from '../../../../core/store/collections/collection';
 import {I18n} from '@ngx-translate/i18n-polyfill';
+import PatchCurrentUser = UsersAction.PatchCurrentUser;
 
 @Component({
   selector: 'user-menu',
@@ -60,6 +58,7 @@ export class UserMenuComponent {
   public url$: Observable<string>;
   public freePlan$: Observable<boolean>;
 
+  private starting: boolean = false;
   private driver: Driver;
 
   public constructor(
@@ -75,10 +74,6 @@ export class UserMenuComponent {
       opacity: 0.5,
       closeBtnText: 'Dismiss',
       onReset: () => this.dismissWizard(),
-    });
-    setTimeout(() => {
-      // trick to find elements in the whole page
-      this.defineSteps();
     });
 
     this.currentUser$ = this.store$.pipe(select(selectCurrentUser));
@@ -128,21 +123,52 @@ export class UserMenuComponent {
 
   public onStartTour(): void {
     this.recallWizard();
-    this.startTour();
+    this.startTour(true);
   }
 
-  private startTour(): void {
+  private startTour(manual?: boolean): void {
+    if (!this.starting) {
+      this.starting = true;
+
+      if (manual) {
+        // we need to make sure to be on the home page
+        this.store$
+          .pipe(
+            select(selectWorkspace),
+            first()
+          )
+          .subscribe(workspace => {
+            this.router
+              .navigate(['/', 'w', workspace.organizationCode, workspace.projectCode, 'view', 'search', 'all'])
+              .then(() => {
+                this.kickstartTour();
+              });
+          });
+      } else {
+        // we already checked the url
+        this.kickstartTour();
+      }
+    }
+  }
+
+  private kickstartTour() {
     setTimeout(() => {
+      // trick to allow access to all document elements
+      this.driver.reset(true);
+      this.defineSteps();
       this.driver.start();
+      this.starting = false;
     }, 500);
   }
 
   public dismissWizard(): void {
-    this.store$.dispatch(
-      new PatchCurrentUser({
-        user: {wizardDismissed: true},
-      })
-    );
+    if (!this.starting) {
+      this.store$.dispatch(
+        new PatchCurrentUser({
+          user: {wizardDismissed: true},
+        })
+      );
+    }
   }
 
   public recallWizard(): void {
@@ -154,20 +180,31 @@ export class UserMenuComponent {
   }
 
   private defineSteps(): void {
+    const welcomeTitle = this.i18n({
+      id: 'appTour.title.welcome',
+      value: 'Welcome to Lumeer',
+    });
+    const welcomeDescription = this.i18n({
+      id: 'appTour.description.welcome',
+      value:
+        '(1/7) Lumeer organizes your information in collections (i.e. categories) of similar records. Later, you can use this button to add your first collection. Then you can open the collection by simply clicking on it.',
+    });
+
     this.driver.defineSteps([
       {
         element: '[data-tour="collection-add"]',
         popover: {
-          title: this.i18n({
-            id: 'appTour.title.welcome',
-            value: 'Welcome to Lumeer',
-          }),
-          description: this.i18n({
-            id: 'appTour.description.welcome',
-            value:
-              '(1/7) Lumeer organizes your information in collections (i.e. categories) of similar records. Later, you can use this button to add your first collection. Then you can open the collection by simply clicking on it.',
-          }),
+          title: welcomeTitle,
+          description: welcomeDescription,
           position: 'right',
+        },
+      },
+      {
+        element: '[data-tour="collection-create"]',
+        popover: {
+          title: welcomeTitle,
+          description: welcomeDescription,
+          position: 'bottom',
         },
       },
       {
@@ -180,7 +217,7 @@ export class UserMenuComponent {
           description: this.i18n({
             id: 'appTour.description.home',
             value:
-              'By clicking on the Lumeer icon, you can always return to this page where you can best find and access your stored data.',
+              '(2/7) By clicking on the Lumeer icon, you can always return to this page where you can best find and access your stored data.',
           }),
           position: 'right',
         },
@@ -195,7 +232,7 @@ export class UserMenuComponent {
           description: this.i18n({
             id: 'appTour.description.search',
             value:
-              'The best way to locate your stored information is to search for it. Lumeer will guide you, just start typing in the search box.',
+              '(3/7) The best way to locate your stored information is to search for it. Lumeer will guide you, just start typing in the search box.',
           }),
           position: 'bottom',
         },
@@ -210,7 +247,7 @@ export class UserMenuComponent {
           description: this.i18n({
             id: 'appTour.description.perspectives',
             value:
-              'When you open your collection or search results, try selecting a different visual perspective. This is similar to changing glasses through which you can see your information in various ways.',
+              '(4/7) When you open your collection or search results, try selecting a different visual perspective. This is similar to changing glasses through which you can see your information in various ways.',
           }),
           position: 'right',
         },
@@ -225,7 +262,7 @@ export class UserMenuComponent {
           description: this.i18n({
             id: 'appTour.description.views',
             value:
-              "You might connect multiple collections in relations, select a Table perspective and hide some columns for example. You don't need to repeat the steps each time. Just give your view, or page if you will, a name and store it. Later you can access the stored view on home page or you can share the view with your colleagues.",
+              "(5/7) You might connect multiple collections in relations, select a Table perspective and hide some columns for example. You don't need to repeat the steps each time. Just give your view, or page if you will, a name and store it. Later you can access the stored view on home page or you can share the view with your colleagues.",
           }),
           position: 'bottom',
         },
@@ -239,7 +276,7 @@ export class UserMenuComponent {
           }),
           description: this.i18n({
             id: 'appTour.description.videos',
-            value: 'Here you can find a list of one-minute videos that explain controls on each page.',
+            value: '(6/7) Here you can find a list of one-minute videos that explain controls on each page.',
           }),
           position: 'left',
         },
@@ -253,7 +290,7 @@ export class UserMenuComponent {
           }),
           description: this.i18n({
             id: 'appTour.description.userMenu',
-            value: 'You can always return to this tour by invoking it from the user menu.',
+            value: '(7/7) You can always return to this tour by invoking it from the user menu.',
           }),
           position: 'left',
         },
