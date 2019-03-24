b5ea7d35e67240eff52f19237db56ccba2be0cae
Jakub Rodák <kubedo8@gmail.com>
2019-03-24 10:31:55 +0100

    removed unexisting function

--- src/app/shared/data-input/percentage/percentage-data-input.component.ts
+++ src/app/shared/data-input/percentage/percentage-data-input.component.ts
@@ -32,7 +32,7 @@ import {
 import {PercentageConstraintConfig} from '../../../core/model/data/constraint';
 import {HtmlModifier} from '../../utils/html-modifier';
 import {KeyCode} from '../../key-code';
-import {isPercentageValid, getPercentageValue, getPercentageSaveValue} from '../../utils/data.utils';
+import {isPercentageValid, getPercentageSaveValue} from '../../utils/data.utils';
 import {PercentageDataValuePipe} from '../../pipes/data/percentage-data-value.pipe';
 
 @Component({
