b49c11ecb5451841384974b21c4541b0caff702e
Martin Večeřa <marvenec@gmail.com>
2019-03-27 01:15:01 +0100

    Update FontAwesome

---
+++ src/assets/font-awesome/fa-parse.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+node fa-parse.js
+
+sed -i '' 's/^/  /' icons
+sed -i '' 's/^/  /' brands
+sed -i '' 's/^/  /' meta
+
+cat > icons.ts <<EOF
+/*
+ * Lumeer: Modern Data Definition and Processing Platform
+ *
+ * Copyright (C) since 2017 Answer Institute, s.r.o. and/or its affiliates.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+export const icons: string[] = [
+EOF
+
+cat icons >> icons.ts
+
+cat >> icons.ts <<EOF
+];
+
+export const solid: string[] = icons.map(color => \`fas \${color}\`);
+
+export const regular: string[] = icons.map(color => \`far \${color}\`);
+
+export const light: string[] = icons.map(color => \`fal \${color}\`);
+
+export const brand: string[] = [
+EOF
+
+cat brands >> icons.ts
+
+cat >> icons.ts <<EOF
+];
+
+export const iconsMeta = new Map<string, string[]>([
+EOF
+
+cat meta >> icons.ts
+
+cat >> icons.ts <<EOF
+]);
+
+export function getPureIconName(icon: string): string {
+  return icon.substring(icon.indexOf('-') + 1);
+}
+
+export function searchIconsByMeta(meta: string): string[] {
+  const result = [];
+  iconsMeta.forEach((iconMeta, key) => {
+    if (key.indexOf(meta) >= 0) {
+      iconMeta.forEach(icon => {
+        if (result.indexOf(icon) < 0) {
+          result.push(icon);
+        }
+      });
+    }
+  });
+
+  return result;
+}
+
+export function safeGetRandomIcon(): string {
+  const unsafe = [
+    'fa-coffin',
+    'fa-book-dead',
+    'fa-skull-crossbones',
+    'fa-tombstone',
+    'fa-tombstone-alt',
+    'fa-flask-poison',
+    'fa-hockey-mask',
+    'fa-genderless',
+    'fa-mars-double',
+    'fa-mars-stroke',
+    'fa-mars-stroke-h',
+    'fa-mars-stroke-v',
+    'fa-transgender',
+    'fa-transgender-alt',
+    'fa-venus-double',
+    'fa-angry',
+    'fa-frown',
+    'fa-frown-open',
+    'fa-sad-tear',
+    'fa-sed-cry',
+    'fa-tired',
+    'fa-poo',
+  ];
+  let icon: string;
+  do {
+    icon = icons[Math.round(Math.random() * icons.length)];
+  } while (unsafe.indexOf(icon) > -1);
+
+  return 'fas ' + icon;
+}
+EOF
