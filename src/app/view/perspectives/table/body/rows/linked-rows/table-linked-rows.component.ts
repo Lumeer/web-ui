48a0fa226d337a6cf9b375f04581b173b786d553
Tomáš Livora <livoratom@gmail.com>
2019-04-12 13:02:14 +0200

    Fix blinking of linked table rows #LMR-1647

--- src/app/view/perspectives/table/body/rows/linked-rows/table-linked-rows.component.ts
+++ src/app/view/perspectives/table/body/rows/linked-rows/table-linked-rows.component.ts
@@ -17,10 +17,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {select, Store} from '@ngrx/store';
-import {Observable} from 'rxjs';
-import {debounceTime, map, tap} from 'rxjs/operators';
+import {BehaviorSubject, combineLatest, Observable} from 'rxjs';
+import {debounceTime, filter, map, switchMap, tap} from 'rxjs/operators';
 import {LinkInstance} from '../../../../../../core/store/link-instances/link.instance';
 import {selectLinkInstancesByDocumentIds} from '../../../../../../core/store/link-instances/link-instances.state';
 import {TableBodyCursor} from '../../../../../../core/store/tables/table-cursor';
@@ -34,7 +34,7 @@ import {TablesAction} from '../../../../../../core/store/tables/tables.action';
   styleUrls: ['./table-linked-rows.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TableLinkedRowsComponent implements OnChanges {
+export class TableLinkedRowsComponent implements OnInit, OnChanges {
   @Input()
   public cursor: TableBodyCursor;
 
@@ -47,31 +47,46 @@ export class TableLinkedRowsComponent implements OnChanges {
   @Input()
   public striped: boolean;
 
+  private cursor$ = new BehaviorSubject(null);
+  private rows$ = new BehaviorSubject([]);
+
   public linkedRows$: Observable<TableConfigRow[]>;
   public emptyLinkedRow = createEmptyTableRow();
 
   constructor(private store$: Store<{}>) {}
 
+  public ngOnInit() {
+    this.linkedRows$ = this.bindLinkedRows();
+  }
+
   public ngOnChanges(changes: SimpleChanges) {
+    if (changes.cursor && this.cursor) {
+      this.cursor$.next(this.cursor);
+    }
     if (changes.rows && this.rows) {
-      this.linkedRows$ = this.bindLinkedRows(this.rows, this.cursor);
+      this.rows$.next(this.rows);
     }
   }
 
-  private bindLinkedRows(rows: TableConfigRow[], cursor: TableBodyCursor): Observable<TableConfigRow[]> {
-    const documentIds = rows.map(row => row.documentId);
-    const linkedRows = rows.reduce((allLinkedRows, row) => allLinkedRows.concat(row.linkedRows), []);
-    return this.store$.pipe(
-      select(selectLinkInstancesByDocumentIds(documentIds)),
-      debounceTime(100), // otherwise unwanted parallel syncing occurs
-      map(linkInstances => filterRowsByExistingLinkInstance(linkedRows, linkInstances)),
-      tap(() =>
-        this.store$.dispatch(
-          new TablesAction.SyncLinkedRows({
-            cursor: {...cursor, partIndex: cursor.partIndex + 1},
-          })
-        )
-      )
+  private bindLinkedRows(): Observable<TableConfigRow[]> {
+    return combineLatest(this.cursor$, this.rows$).pipe(
+      filter(([cursor, rows]) => cursor && rows.length > 0),
+      switchMap(([cursor, rows]) => {
+        const documentIds = rows.map(row => row.documentId);
+        const linkedRows = rows.reduce((allLinkedRows, row) => allLinkedRows.concat(row.linkedRows), []);
+        return this.store$.pipe(
+          select(selectLinkInstancesByDocumentIds(documentIds)),
+          debounceTime(100), // otherwise unwanted parallel syncing occurs
+          map(linkInstances => filterRowsByExistingLinkInstance(linkedRows, linkInstances)),
+          tap(() =>
+            this.store$.dispatch(
+              new TablesAction.SyncLinkedRows({
+                cursor: {...cursor, partIndex: cursor.partIndex + 1},
+              })
+            )
+          )
+        );
+      })
     );
   }
 
