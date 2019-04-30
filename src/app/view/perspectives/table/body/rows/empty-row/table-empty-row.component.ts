bb05be66441a089e8b1d7713f9844f7c8e75a73d
Tomáš Livora <livoratom@gmail.com>
2019-04-30 08:49:10 +0200

    Show no results row in table #LMR-1659

---
+++ src/app/view/perspectives/table/body/rows/empty-row/table-empty-row.component.ts
@@ -0,0 +1,71 @@
+/*
+ * Lumeer: Modern Data Definition and Processing Platform
+ *
+ * Copyright (C) since 2017 Answer Institute, s.r.o. and/or its affiliates.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {ChangeDetectionStrategy, Component, ElementRef, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {select, Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {TableBodyCursor} from '../../../../../../core/store/tables/table-cursor';
+import {calculateColumnsWidth} from '../../../../../../core/store/tables/table.utils';
+import {selectTableParts} from '../../../../../../core/store/tables/tables.selector';
+
+@Component({
+  selector: 'table-empty-row',
+  templateUrl: './table-empty-row.component.html',
+  styleUrls: ['./table-empty-row.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TableEmptyRowComponent implements OnChanges {
+  @Input()
+  public canManageConfig: boolean;
+
+  @Input()
+  public cursor: TableBodyCursor;
+
+  public dataColumnsWidth$: Observable<number>;
+  public linkInfoCells$: Observable<any[]>;
+
+  constructor(private element: ElementRef<HTMLElement>, private store$: Store<{}>) {}
+
+  public ngOnChanges(changes: SimpleChanges) {
+    if ((changes.canManageConfig || changes.cursor) && this.cursor) {
+      this.dataColumnsWidth$ = this.bindDataColumnsWidth();
+      this.linkInfoCells$ = this.bindLinkInfoCells();
+    }
+  }
+
+  private bindDataColumnsWidth(): Observable<number> {
+    return this.store$.pipe(
+      select(selectTableParts(this.cursor)),
+      map(
+        parts =>
+          parts &&
+          parts.reduce((width, part) => width + calculateColumnsWidth(part.columns, this.canManageConfig), 0) - 1
+      )
+    );
+  }
+
+  private bindLinkInfoCells(): Observable<any[]> {
+    return this.store$.pipe(
+      select(selectTableParts(this.cursor)),
+      map(parts => (parts ? Math.floor(parts.length / 2) : 0)),
+      map(count => new Array(count))
+    );
+  }
+}
