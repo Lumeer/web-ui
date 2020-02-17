460eb0fff99ecafbe449676e95217f3a1a1b25ae
Martin Večeřa <marvec@users.noreply.github.com>
2020-02-17 20:48:22 +0100

    Add docker trigger for master branch (#1184)

--- docker-trigger.sh
+++ docker-trigger.sh
@@ -9,6 +9,7 @@ if [ "x$TRAVIS_PULL_REQUEST" = "xfalse" -o -z "$TRAVIS_PULL_REQUEST" ]; then
   fi
   if [ "x$TRAVIS_BRANCH" = "xmaster" ]; then
     echo Trigger master rebuild
+    curl -X POST -is -u "${BB_AUTH_STRING}" -H "${CONTENT_TYPE}" https://api.bitbucket.org/2.0/repositories/lumeer/app-prod-v2/pipelines/ -d "$(sed 's/\$VERSION/'"$VERSION"'/' < docker-pipeline-request.txt)"
   fi
 else
   echo "Skipping trigger for PR"
