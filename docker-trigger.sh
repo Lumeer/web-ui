1bd134bbd7d4bbfaced7aafc647af82b680a7279
Martin Večeřa <mvecera@lumeer.io>
2020-02-18 16:24:46 +0100

    Mask curl response from server

--- docker-trigger.sh
+++ docker-trigger.sh
@@ -5,11 +5,11 @@ VERSION=$(git log --pretty=format:'%h' -n 1)
 if [ "x$TRAVIS_PULL_REQUEST" = "xfalse" -o -z "$TRAVIS_PULL_REQUEST" ]; then
   if [ "x$TRAVIS_BRANCH" = "xdevel" ]; then
     echo Trigger devel rebuild
-    curl -X POST -is -u "${BB_AUTH_STRING}" -H "Content-Type: application/json" https://api.bitbucket.org/2.0/repositories/lumeer/app-devel-v2/pipelines/ -d "$(sed 's/\$VERSION/'"$VERSION"'/' < docker-pipeline-request.txt)"
+    curl -X POST -is -u "${BB_AUTH_STRING}" -H "Content-Type: application/json" https://api.bitbucket.org/2.0/repositories/lumeer/app-devel-v2/pipelines/ -d "$(sed 's/\$VERSION/'"$VERSION"'/' < docker-pipeline-request.txt)" > /dev/null
   fi
   if [ "x$TRAVIS_BRANCH" = "xmaster" ]; then
     echo Trigger master rebuild
-    curl -X POST -is -u "${BB_AUTH_STRING}" -H "Content-Type: application/json" https://api.bitbucket.org/2.0/repositories/lumeer/app-prod-v2/pipelines/ -d "$(sed 's/\$VERSION/'"$VERSION"'/' < docker-pipeline-request.txt)"
+    curl -X POST -is -u "${BB_AUTH_STRING}" -H "Content-Type: application/json" https://api.bitbucket.org/2.0/repositories/lumeer/app-prod-v2/pipelines/ -d "$(sed 's/\$VERSION/'"$VERSION"'/' < docker-pipeline-request.txt)" > /dev/null
   fi
 else
   echo "Skipping trigger for PR"
