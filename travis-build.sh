0834614fef4aff74aec45d6a089a67ced63511e8
Martin Večeřa <marvenec@gmail.com>
2020-03-22 17:08:05 +0100

    Add travis build script with liveness check

---
+++ travis-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Abort on Error
+set -e
+
+# Set up a repeating loop to send some output to Travis.
+export PING_SLEEP=300s
+bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
+export PING_LOOP_PID=$!
+
+LUMEER_ENV=testing NODE_OPTIONS=--max_old_space_size=7000 mvn clean install -Dcontext.root=/
+
+kill $PING_LOOP_PID
