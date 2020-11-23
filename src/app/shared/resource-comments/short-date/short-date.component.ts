53971fd5b5c6812c29eaa3cfcf6f1a78d13377b3
Martin Večeřa <marvenec@gmail.com>
2020-11-23 13:27:07 +0100

    Add basic comments components

---
+++ src/app/shared/resource-comments/short-date/short-date.component.ts
@@ -0,0 +1,39 @@
+/*
+ * Lumeer: Modern Data Definition and Processing Platform
+ *
+ * Copyright (C) since 2017 Lumeer.io, s.r.o. and/or its affiliates.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'short-date',
+  templateUrl: './short-date.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ShortDateComponent {
+  @Input()
+  public date: Date;
+
+  @Input()
+  public format = 'lll';
+
+  @Input()
+  public icon: string;
+
+  @Input()
+  public tooltipText: string;
+}
