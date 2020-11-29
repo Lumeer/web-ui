3f8618ed453d0afd4556332b3b20f45d2e738bd0
Jakub <kubedo8@gmail.com>
2020-11-29 14:14:25 +0100

    console.log(index, x, y);

--- src/app/view/perspectives/chart/visualizer/plot-maker/pie-plot-maker.ts
+++ src/app/view/perspectives/chart/visualizer/plot-maker/pie-plot-maker.ts
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {Layout, PlotType} from 'plotly.js';
+import {Annotations, Layout, PlotType} from 'plotly.js';
 import {ChartAxisType} from '../../../../../core/store/charts/chart';
 import {isNotNullOrUndefined} from '../../../../../shared/utils/common.utils';
 import {ChartDataSet} from '../../data/convertor/chart-data';
@@ -26,6 +26,8 @@ import {ConstraintType} from '../../../../../core/model/data/constraint';
 import {uniqueValues} from '../../../../../shared/utils/array.utils';
 
 const MAX_COLUMNS = 3;
+const HORIZONTAL_SPACING = 0.04;
+const VERTICAL_SPACING = 0.08;
 
 export class PiePlotMaker extends PlotMaker {
   public createData(): PlotlyChartData[] {
@@ -38,11 +40,14 @@ export class PiePlotMaker extends PlotMaker {
     }
 
     const columns = Math.min(sets.length, MAX_COLUMNS);
-
+    const rows = numRows(sets);
     return sets.map((set, index) => {
-      const row = sets.length > 1 ? Math.floor(index / MAX_COLUMNS) : null;
-      const column = sets.length > 1 ? index % columns : null;
-      return this.createAxesData(dataStyle, set, row, column);
+      let x, y;
+      if (sets.length) {
+        x = computePieXRange(index, columns);
+        y = computePieYRange(index, rows);
+      }
+      return this.createAxesData(dataStyle, set, x, y);
     });
   }
 
@@ -76,12 +81,7 @@ export class PiePlotMaker extends PlotMaker {
     // nothing to do
   }
 
-  private createAxesData(
-    dataStyle: PlotlyChartData,
-    set: ChartDataSet,
-    row?: number,
-    column?: number
-  ): PlotlyChartData {
+  private createAxesData(dataStyle: PlotlyChartData, set: ChartDataSet, x?: number[], y?: number[]): PlotlyChartData {
     const labels = [];
     const values = [];
     const colors = [];
@@ -99,8 +99,8 @@ export class PiePlotMaker extends PlotMaker {
       data.marker.colors = colors;
     }
 
-    if (isNotNullOrUndefined(row) && isNotNullOrUndefined(column)) {
-      data.domain = {rows: row, columns: column};
+    if (isNotNullOrUndefined(x) && isNotNullOrUndefined(y)) {
+      data.domain = {x, y};
     }
 
     return data;
@@ -113,10 +113,65 @@ export class PiePlotMaker extends PlotMaker {
   public createLayout(): Partial<Layout> {
     const sets = this.getSets();
     if (sets.length > 1) {
-      const rows = Math.floor((sets.length - 1) / MAX_COLUMNS) + 1;
-      const columns = Math.min(sets.length, MAX_COLUMNS);
-      return {grid: {rows, columns}};
+      const rows = numRows(sets);
+      const columns = numColumns(sets);
+      const annotations: Partial<Annotations>[] = sets.map((set, index) => {
+        const x = computePieXRange(index, columns);
+        const y = computePieYRange(index, rows);
+        return {
+          text: `<b>${set.name || ''}</b>`,
+          showarrow: false,
+          x: x[0] + (x[1] - x[0]) / 2,
+          y: y[1] + VERTICAL_SPACING / 2,
+          font: {size: 16},
+          xref: 'paper',
+          yref: 'paper',
+          xanchor: 'center',
+          yanchor: 'middle',
+        };
+      });
+      return {grid: {rows, columns}, annotations};
     }
     return {};
   }
+}
+
+function numRows(sets: ChartDataSet[]): number {
+  return Math.floor((sets.length - 1) / MAX_COLUMNS) + 1;
+}
+
+function numColumns(sets: ChartDataSet[]): number {
+  return Math.min(sets.length, MAX_COLUMNS);
+}
+
+function computePieSize(parts: number, spacingBetween: number, spacingBefore = 0): number {
+  return (1 - (parts - 1) * spacingBetween - spacingBefore * 2) / parts;
+}
+
+function computePieXRange(index: number, columns: number): [number, number] {
+  const spacing = HORIZONTAL_SPACING;
+  const pieWidth = computePieSize(columns, spacing);
+
+  const column = columnIndex(index, columns);
+  const x1 = column * pieWidth + column * spacing;
+  const x2 = (column + 1) * pieWidth + column * spacing;
+  return [x1, Math.min(x2, 1)];
+}
+
+function computePieYRange(index: number, columns: number): [number, number] {
+  const spacing = VERTICAL_SPACING;
+  const pieWidth = computePieSize(columns, spacing, spacing / 2);
+
+  const row = rowIndex(index);
+  const y1 = row * pieWidth + (row + 1 / 2) * spacing;
+  const y2 = (row + 1) * pieWidth + (row + 1 / 2) * spacing;
+  return [1 - Math.min(y2, 1), 1 - y1];
+}
+
+function rowIndex(index: number): number {
+  return Math.floor(index / MAX_COLUMNS);
+}
+
+function columnIndex(index: number, columns: number): number {
+  return index % columns;
 }
