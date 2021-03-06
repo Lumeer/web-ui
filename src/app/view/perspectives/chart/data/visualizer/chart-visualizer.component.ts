decdea0cf5cbbd993c606d8dab7d36943ecf2974
Jakub <kubedo8@gmail.com>
2021-03-06 14:02:44 +0100

    revert console log

--- src/app/view/perspectives/chart/data/visualizer/chart-visualizer.component.ts
+++ src/app/view/perspectives/chart/data/visualizer/chart-visualizer.component.ts
@@ -81,7 +81,6 @@ export class ChartVisualizerComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   private visualize() {
-    console.log(this.chartData);
     if (this.chartVisualizer) {
       this.refreshChart();
     } else {
