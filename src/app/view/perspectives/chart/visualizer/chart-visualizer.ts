cf9ce712672f781cc43432ed8e9e8c7bc6d18654
Jakub <kubedo8@gmail.com>
2021-03-25 17:09:39 +0100

    Fix chart locale

--- src/app/view/perspectives/chart/visualizer/chart-visualizer.ts
+++ src/app/view/perspectives/chart/visualizer/chart-visualizer.ts
@@ -18,6 +18,9 @@
  */
 
 import {ElementRef} from '@angular/core';
+import PlotlyJS from 'plotly.js';
+import CSLocale from 'plotly.js/lib/locales/cs.js';
+import HULocale from 'plotly.js/lib/locales/hu.js';
 import {Config, d3, Layout, newPlot, PlotData, PlotRelayoutEvent, Plots, purge, react} from 'plotly.js';
 import {ChartAxisType, ChartType} from '../../../../core/store/charts/chart';
 import {DataChange, PlotlyChartData, PlotMaker} from './plot-maker/plot-maker';
@@ -30,6 +33,7 @@ import {BubblePlotMaker} from './plot-maker/bubble-plot-maker';
 import {AttributesResourceType} from '../../../../core/model/resource';
 import {deepArrayEquals} from '../../../../shared/utils/array.utils';
 import {COLOR_PRIMARY} from '../../../../core/constants';
+import {LanguageCode} from '../../../../shared/top-panel/user-panel/user-menu/language';
 
 export class ChartVisualizer {
   private currentType: ChartType;
@@ -68,6 +72,19 @@ export class ChartVisualizer {
 
   public setLocale(locale: string) {
     this.locale = locale;
+    this.registerLanguage(locale);
+    this.config = this.createConfig();
+  }
+
+  private registerLanguage(locale: string) {
+    switch (locale) {
+      case LanguageCode.CZ:
+        (PlotlyJS as any).register(CSLocale);
+        break;
+      case LanguageCode.HU:
+        (PlotlyJS as any).register(HULocale);
+        break;
+    }
   }
 
   public createChart(data: ChartData, settings: ChartSettings) {
