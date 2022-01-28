36e09440cf108085a3ff600faab86a7aef8474a9
Jakub <kubedo8@gmail.com>
2022-01-28 08:43:06 +0100

    Try to fix tests

--- cypress/integration/perspective/search/search-collections.spec.js
+++ cypress/integration/perspective/search/search-collections.spec.js
@@ -2,7 +2,7 @@ describe('Search Perspective :: Collections', () => {
   it('creates first collection', () => {
     cy.dismissAppTour();
 
-    cy.visit('/');
+    cy.visit('/', {timeout: 10000});
     cy.url().should('contain', '/view/search/all');
 
     cy.get('[data-test="empty-project-header"]').should('be.visible');
