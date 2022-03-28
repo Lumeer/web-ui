43d317b3aea70863a9678fc929168ec93834fd56
Martin Večeřa <marvec@users.noreply.github.com>
2022-03-28 15:09:35 +0200

    Get user teams (#1932)
    * Add block to get user teams
    
    * Add blocks to get user teams
    
    * Complete transaltions

---
+++ src/app/shared/blockly/blockly-editor/blocks/get-user-team-ids-blockly-component.ts
@@ -0,0 +1,69 @@
+/*
+ * Lumeer: Modern Data Definition and Processing Platform
+ *
+ * Copyright (C) since 2017 Lumeer.io, s.r.o. and/or its affiliates.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {BlocklyComponent} from './blockly-component';
+import {BlocklyUtils, MasterBlockType} from '../blockly-utils';
+import {COLOR_CYAN} from '../../../../core/constants';
+
+declare var Blockly: any;
+
+export class GetUserTeamIdsBlocklyComponent extends BlocklyComponent {
+  private tooltip: string;
+
+  public constructor(public blocklyUtils: BlocklyUtils) {
+    super(blocklyUtils);
+
+    this.tooltip = $localize`:@@blockly.tooltip.getUserTeamIdsBlock:Get team IDs the specified user is member of. The user is specified using their email.`;
+  }
+
+  public getVisibility(): MasterBlockType[] {
+    return [MasterBlockType.Rule, MasterBlockType.Link, MasterBlockType.Function];
+  }
+
+  public registerBlock(workspace: any) {
+    const this_ = this;
+
+    Blockly.Blocks[BlocklyUtils.GET_USER_TEAM_IDS] = {
+      init: function () {
+        this.jsonInit({
+          type: BlocklyUtils.GET_USER_TEAM_IDS,
+          message0: 'get user team IDs %1', //'%{BKY_BLOCK_GET_USER_TEAM_IDS}', // current teams
+          args0: [
+            {
+              type: 'input_value',
+              name: 'USER_EMAIL',
+            },
+          ],
+          output: '',
+          colour: COLOR_CYAN,
+          tooltip: this_.tooltip,
+          helpUrl: '',
+        });
+      },
+    };
+    Blockly.JavaScript[BlocklyUtils.GET_USER_TEAM_IDS] = function (block) {
+      const argument0 =
+        Blockly.JavaScript.valueToCode(block, 'USER_EMAIL', Blockly.JavaScript.ORDER_ASSIGNMENT) || null;
+
+      const code = this_.blocklyUtils.getLumeerVariable() + `.getUserTeamIds(${argument0})`;
+
+      return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+    };
+  }
+}
---
+++ src/app/shared/blockly/blockly-editor/blocks/get-user-teams-blockly-component.ts
@@ -0,0 +1,69 @@
+/*
+ * Lumeer: Modern Data Definition and Processing Platform
+ *
+ * Copyright (C) since 2017 Lumeer.io, s.r.o. and/or its affiliates.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {BlocklyComponent} from './blockly-component';
+import {BlocklyUtils, MasterBlockType} from '../blockly-utils';
+import {COLOR_CYAN} from '../../../../core/constants';
+
+declare var Blockly: any;
+
+export class GetUserTeamsBlocklyComponent extends BlocklyComponent {
+  private tooltip: string;
+
+  public constructor(public blocklyUtils: BlocklyUtils) {
+    super(blocklyUtils);
+
+    this.tooltip = $localize`:@@blockly.tooltip.getUserTeamsBlock:Get teams the specified user is member of. Returns list, use "make text from list" to display or store the value. The user is specified using their email.`;
+  }
+
+  public getVisibility(): MasterBlockType[] {
+    return [MasterBlockType.Rule, MasterBlockType.Link, MasterBlockType.Function];
+  }
+
+  public registerBlock(workspace: any) {
+    const this_ = this;
+
+    Blockly.Blocks[BlocklyUtils.GET_USER_TEAMS] = {
+      init: function () {
+        this.jsonInit({
+          type: BlocklyUtils.GET_USER_TEAMS,
+          message0: 'get user teams %1', //'%{BKY_BLOCK_GET_USER_TEAMS}', // current teams
+          args0: [
+            {
+              type: 'input_value',
+              name: 'USER_EMAIL',
+            },
+          ],
+          output: '',
+          colour: COLOR_CYAN,
+          tooltip: this_.tooltip,
+          helpUrl: '',
+        });
+      },
+    };
+    Blockly.JavaScript[BlocklyUtils.GET_USER_TEAMS] = function (block) {
+      const argument0 =
+        Blockly.JavaScript.valueToCode(block, 'USER_EMAIL', Blockly.JavaScript.ORDER_ASSIGNMENT) || null;
+
+      const code = this_.blocklyUtils.getLumeerVariable() + `.getUserTeams(${argument0})`;
+
+      return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+    };
+  }
+}
