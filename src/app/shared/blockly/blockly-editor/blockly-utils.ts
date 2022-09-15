39926561246012bcecf67b7aca59435ca171cc1b
Martin Večeřa <marvec@users.noreply.github.com>
2022-09-15 23:32:32 +0200

    Fix get_link_instance block (#1987)

--- src/app/shared/blockly/blockly-editor/blockly-utils.ts
+++ src/app/shared/blockly/blockly-editor/blockly-utils.ts
@@ -1150,6 +1150,7 @@ export class BlocklyUtils {
                 check: [
                   linkType.collectionIds[0] + BlocklyUtils.DOCUMENT_VAR_SUFFIX,
                   linkType.collectionIds[1] + BlocklyUtils.DOCUMENT_VAR_SUFFIX,
+                  BlocklyUtils.GET_LINK_DOCUMENT_UNKNOWN,
                 ],
               },
             ],
