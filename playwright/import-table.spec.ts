105e7ec72075f220e6e4787883125911dff6f45b
rodak <kubedo8@gmail.com>
2023-11-21 16:57:02 +0100

    Run prettier

--- playwright/import-table.spec.ts
+++ playwright/import-table.spec.ts
@@ -16,8 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-import {test, expect} from '@playwright/test';
+import {expect, test} from '@playwright/test';
 
 test.describe.configure({mode: 'serial'});
 
